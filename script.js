08b8ecdff45a1326e3e4f017b27a1b2d2594d02e
BuickSalesman <adam.odonnell.e@gmail.com>
2025-02-11 19:03:11 -0500

    added shrinkage

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const gameContainer = document.getElementById("game-container");
 let target = document.getElementById("target");
 
+let isStarted = false;
+let bullseyes = 0;
+let targetHeight = 100;
+let targetWidth = 100;
+
 target.addEventListener("click", () => {
+  bullseyes++;
   generateRandomTargetPosition();
+  if (!isStarted) {
+    startTimer();
+  }
+
+  shrinkTarget();
 });
 
 function generateRandomTargetPosition() {
@@ -17,4 +28,18 @@ function getTwoRandomNumbers() {
 
   console.log(number1, number2);
   return [number1, number2];
+}
+
+function startTimer() {
+  return;
+}
+
+function shrinkTarget() {
+  if (bullseyes % 2 === 0) {
+    targetHeight = targetHeight * 0.75;
+    targetWidth = targetWidth * 0.75;
+    target.style.height = `${targetHeight}px`;
+    target.style.width = `${targetWidth}px`;
+    target.textContent = null;
+  }
 }
